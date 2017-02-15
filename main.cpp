8d52742582b230cbb3575d4ba57253d1245dee30
josayarh <johakim.sayarh@gmail.com>
2017-02-14 21:16:09 -0600

    petits changements

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <thread>
 #include "Environnement.h"
 #include "Agent.h"
 #include <unistd.h>
+
+void threadFunc(Environnement &environnement){
+    Agent agent = Agent(environnement.getCarte(),environnement);
+    while(environnement.gameIsRunning()){
+        agent.observe();
+        agent.chooseBelief();
+        agent.chooseDesire();
+        agent.DOITNOW();
+        sleep(1);
+
+        if(agent.getBattery()==0){
+            cout << "WARNING : Agent has no enough battery !!, end of application :(" <<  endl;
+            break;
+
+        }
+    }
+}
 
 
 int main() {
@@ -9,10 +27,9 @@ int main() {
     Carte &agentMap = environnement.getCarte();
     Carte &carte = environnement.getCarte();
 
-    // TEST de l'algo d'exploration A*
-    Agent agent = Agent(agentMap,environnement);
+    std::thread thread1(threadFunc,std::ref(environnement));
+    thread1.detach();
 
-    int cmpExploration = 0;
 
     while(environnement.gameIsRunning()){
         cout << "Nouveau calcul : " << endl;
@@ -25,29 +42,7 @@ int main() {
         }
         cout << carte << endl;
 
-        agent.observe();
-        agent.chooseBelief();
-        agent.chooseDesire();
-        agent.DOITNOW();
-
-
-        // Find path to collect all jewel and clean all dirt
-        if(cmpExploration > 2) {
-            agent.setMap(environnement.getCarte());
-            cmpExploration=0;
-        }
-        if(agent.getBattery()==0){
-            cout << "WARNING : Agent has no enough battery !!, end of application :(" <<  endl;
-            break;
-
-        }
-
-//        agent.getEffecteur().move();
-//        cmpExploration++;
-
-
         sleep(1);
-
 
     }
     return 0;
